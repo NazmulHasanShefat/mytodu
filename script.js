1ea493948d34eb61bc1bb19dbff63722904a84c3
NazmulHasanShefat <119728546+NazmulHasanShefat@users.noreply.github.com>
2024-07-09 01:19:55 +0600

    frist commit

---
+++ script.js
@@ -0,0 +1,44 @@
+let input_fuld = document.querySelector(".input_list");
+let submit_btn = document.querySelector(".submit_btn");
+let list_container = document.querySelector(".list_container");
+
+
+submit_btn.addEventListener("click",()=>{
+    let input_value = input_fuld.value;
+    if(input_value < 1){
+         alert("please input your list name");
+    }
+    else{
+        let list = document.createElement("li");
+        list.classList.add("list-group-item");
+        list.classList.add("my_list");
+        list.classList.add("d-flex");
+        list.classList.add("justify-content-between");
+        list.classList.add("align-items-center");
+        list .innerHTML = `
+        <p class="text">${input_value}</p>
+        <button type="button" class="btn-close" aria-label="Close"></button>
+        `
+        list_container.appendChild(list);
+        save_data();
+    }
+})
+
+list_container.addEventListener("click",function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+        save_data();
+    }
+    else if(e.target.tagName === "BUTTON"){
+        e.target.parentElement.remove();
+        save_data();
+    }
+})
+
+function save_data(){
+    localStorage.setItem("data", list_container.innerHTML);
+}
+function gofunction(){
+    list_container.innerHTML = localStorage.getItem("data");
+}
+gofunction();
